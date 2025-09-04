557efaaa8cd2632093716a6d7b938f54707d8d51
Mouaad020 <mouaad.chakroun@hva.nl>
2025-09-04 09:27:01 +0200

    day 3

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,17 @@ wiebenik.addEventListener('click', function(){
 window.location.href = "wiebenik.html"
 console.log('gelukt')
 })
+
+const hobby = document.querySelector('.hobby')
+
+hobby.addEventListener('click', function(){
+window.location.href = "hobby.html"
+console.log('gelukt')
+})
+
+const ervaring = document.querySelector('.ervaring')
+
+ervaring.addEventListener('click', function(){
+window.location.href = "ervaring.html"
+console.log('gelukt')
+})
